f6da7adb8ba205784e0b784de43b2fbcd553d241
jeremy-rodrguez <jeremy.rodrguez@yahoo.com>
2020-12-17 15:52:40 -0800

    Validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ActiveRecord::Base
+    validates_presence_of :name
+    validates_uniqueness_of :name
+    validates :phone_number, length: { is: 10}
 end
+
+# validates :name, presence: true
+# validates :name, uniqueness: true
+# validates :phone_number, length: { is: 10}
